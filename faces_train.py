50b136a975e03236e92e5c56a446400a7b81c520
GraniteMask <ratnadeepastronomy@gmail.com>
2021-07-24 02:57:12 +0530

    face recognizer completed

--- faces_train.py
+++ faces_train.py
@@ -52,6 +52,6 @@ face_recognizer = cv.face.LBPHFaceRecognizer_create()
 
 face_recognizer.train(features, labels)
 
-face_recognizer.save('face_trained.yml')
+face_recognizer.save('face_trained.yml')  # trained file save to use later
 np.save('features.npy', features)
 np.save('labels.npy', labels)
